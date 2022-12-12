d4c60a73f3205f1d7461b6ea3adebf9267d31636
Cuong Le <lequocc31@gmail.com>
2022-12-12 16:23:45 +0700

    JS

--- main.js
+++ main.js
@@ -143,15 +143,63 @@ if(a !== b){
 
 
 //Hiểu  hơn về câu lệnh điều kiện và phép so sánh
-
+/*Trong JS có 6 giá trị sẽ chuyển đổi qua Boolean có kết quả là false:
+    - Giá trị 0
+    - false
+    - Chuỗi rỗng: '' và ""
+    - undefined
+    - NaN
+    - null
+còn lại chuyển đổi qua Boolean có kết quả là true    
+*/
 var a = 1; 
 var b = 2;
 
 var result = a<b;
 console.log('result:' , result);
 
-if(a < b){
-    console.log('A < B');
+var result1 = a<b && a<0 ;
+console.log('result1:' , result1);
+
+var result2 = 'A' && 'B';
+console.log('result2:' , result2);
+/*Khi đó JS kiểm tra từ trái sang phải khi kiểm tra từ trái thì nếu giá trị không thuộc 6 giá trị trên thì sau đó  
+lấy giá trị phải gán vào result nếu có giá trị thuộc 6 giá trị trên thì sẽ lấy giá trị đó và không kiểm tra các phần tử sau  */ 
+
+var result3 = null || 0 || NaN || 'D';
+console.log('result3:' , result3);
+if(result3){
+    console.log('True');
 }else{
-    console.log('A >= B');
+    console.log('False');
 }
+/*Khi đó JS kiểm tra từ trái sang phải nếu phần tử đầu tiên không thuộc 6 giá trị trên thì nó sẽ lấy giá trị đó  và không kiểm tra các phần tử sau đó 
+nếu phần tử đầu tiên  thuộc 6 giá trị trên thì nó sẽ lấy giá trị tiếp theo và không kiểm tra các phẩn tử sau đó 
+nếu tất cả các phần tử thuộc vào 6 giá trị trên thì biểu thức đó là sai */
+
+/*
+CHUỖI TRONG JS
+1. Tạo chuỗi 
+    - Các cách tạo chuỗi 
+    - Nên sử dụng cách nào? Lý do?
+    - Kiểm tra data type
+2. Một số case sử dụng backslash (\)
+3. Xem độ dài chuỗi
+4. Chú ý độ dài khi viết code
+5. Template string ES6
+*/
+
+/*  Tạo Chuỗi
+    Có 2 cách tạo chuỗi 
+    Cách 1: Khuyên dùng */
+var fullName2 = 'Lê Quốc Cường';
+console.log(fullName2);
+//  Cách 2:
+var fullName3 = new String('Lê Quốc Cường');
+console.log(fullName3);
+
+/*2. Một số case sử dụng backslash (\)
+(backslash in JS) */
+var fullName4 = 'Quốc Cường \'SuperMan\'';
+console.log(fullName4);
+
