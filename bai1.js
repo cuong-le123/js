5cb77912656ed0de55b4506b289fbd8369bb3116
Cuong Le <lequocc31@gmail.com>
2022-12-19 15:27:34 +0700

    JS

--- bai1.js
+++ bai1.js
@@ -2,13 +2,59 @@
 //buoc 1: nguoi dung nhap a va b 
 //buoc 2: x = -b/a
 
-var a = 0;
-var b = 6;
+// var a =1;
+// var b =2;
+// function Validator(option){
+
+//     //Hàm thực hiện validate
+//     function validate(inputElement,rule){
+//         var errorMessage = rule.test(inputElement.value);
+//         var errorElement = inputElement.parentElement.querySelector('.form-message');       
+//         if(errorMessage){
+//             errorElement.innerText = errorMessage;
+//             inputElement.parentElement.classList.add('invalid');
+//         }else{
+//             errorElement.innerText = '';
+//             inputElement.parentElement.classList.remove('invalid');
+//         }
+//     }
+//     //Lấy element của form cần validate
+//     var formElement =document.querySelector(option.form);
+//     if(formElement){
+//         option.rules.forEach(function (rule){
+
+//             var inputElement = formElement.querySelector(rule.selector);
+
+//             if(inputElement){
+//                 inputElement.onblur = function(){
+//                     // lấy value người dùng nhập vào: inputElement.value
+//                     // lấy test function: rule.test
+//                     // rule.test();
+//                     // console.log (rule);
+//                     validate(inputElement,rule);
+//                 }
+//             }
+//         });
+//     }
+// }
+// // Định nghĩa rule:
+// // Nguyên tắc của các rule:
+// // 1. Khi có lỗi => trả ra thông báo lỗi 
+// // 2. Khi hợp lệ => trả về Undefined
+// Validator.isRequired = function(selector){
+//     return {
+//         selector: selector,
+//         test: function(value){
+//             return value.trim() ? undefined : 'Vui Lòng nhập số vào đây'
+//         }
+//     }
+// }
+
 
 function tinhphuongtrinhbacnhat(a1,b1) {
     var x;
     if(a1 === 0 && b1 === 0 ) {
-        console.log('Phương trình vô nghiệm.');
+        console.log('Phương trình vô số nghiệm.');
     }else if (a1 !== 0 && b1 === 0) {
         x = -b1/a1;
         console.log('Phương trình có nghiệm:',x);
@@ -20,5 +66,3 @@ function tinhphuongtrinhbacnhat(a1,b1) {
     }
     return x;
 }
-var kq = tinhphuongtrinhbacnhat(a,b);
-console.log(kq);
