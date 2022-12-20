866964510439d90294d40559955595aa34b5d0c0
Cuong Le <lequocc31@gmail.com>
2022-12-20 14:56:50 +0700

    JS

--- bai3.js
+++ bai3.js
@@ -1,19 +1,9 @@
-//giai thừa 
-//nếu a<0 => không có giai thừa
-//nếu a=0 hoặc a=1 => giai thừa = 1
-//nếu a>0 và a!=1 => giai thừa = a*(a-1)
 
-function factorialize(){
-    var a = document.dataform.a.value;//parseFloat: biến kiểu string -> number
-    var ketqua = "Giai thừa của " + a +" là: "; 
-    if (a < 0){
-       ketqua += "Không có giai thừa ";
-    }else if(a === 0||a === 1){
-        ketqua += "Giai thừa của "+ a + " là: 1" ;
-    }else{
-        for(var i; i<=a; i++){
-            ketqua += "Giai thừa của "+ a + " là:" + (i * (i-1));
-        }
+function tong() {
+    var a = document.dataform.a.value;
+    var s = 0
+    for(var i = 1 ; i <= a; i++){
+        s = s + i;
+        document.getElementById("ketqua").innerHTML= s;
     }
-    document.getElementsByClassName("ketqua")[0].innerHTML = ketqua ;
-}
+}   
