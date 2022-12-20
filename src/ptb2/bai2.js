b858e32e3b1dd9f327ac9373dc389e9bbfbbe3e6
Cuong Le <lequocc31@gmail.com>
2022-12-20 19:47:25 +0700

    JS

--- src/ptb2/bai2.js
+++ src/ptb2/bai2.js
@@ -18,25 +18,15 @@ function giaipt() {
   var ketqua = "Phương Trình " + a + "x^2 +(" + b + ")x +(" + c + ")= 0 <br>";
   if (a === 0 && b === 0 && c === 0) {
     ketqua += "Phương trình có vô số nghiệm";
-  }
-
-  if (a === 0 && b === 0 && c !== 0) {
+  }else if (a === 0 && b === 0 && c !== 0) {
     ketqua += "Phương trình vô nghiệm";
-  }
-
-  if (a === 0 && b !== 0 && c === 0) {
+  }else if (a === 0 && b !== 0 && c === 0) {
     ketqua += " Phương trình có nghiệm là 0";
-  }
-
-  if (a !== 0 && b === 0 && c === 0) {
+  }else if (a !== 0 && b === 0 && c === 0) {
     ketqua += "Phương trình có nghiệm là 0";
-  }
-
-  if (a === 0 && b !== 0 && c !== 0) {
+  }else if (a === 0 && b !== 0 && c !== 0) {
     ketqua += "Phương trình có nghiệm x là:" + -c / b;
-  }
-
-  if (a !== 0 && b !== 0 && c !== 0) {
+  }else if (a !== 0 && b !== 0 && c !== 0) {
     const delta = b * b - 4 * a * c;
     if (delta < 0) {
       ketqua += "Phương trình vô nghiệm";
@@ -45,7 +35,7 @@ function giaipt() {
       ketqua += "Phương trình có nghiệm kép" + -b / (2 * a);
     }
     if (delta > 0) {
-      ketqua += "Phương trình có 2 nghiệm phân biệt x1 , x2" ;
+      ketqua += "có 2 nghiệm phân biệt x1 , x2" ;
       x1 = -b + Math.sqrt(delta) / (2 * a);
       x2 = -b - Math.sqrt(delta) / (2 * a);
       document.getElementById("x1").innerHTML = x1; 
@@ -53,5 +43,8 @@ function giaipt() {
     }
   }
   document.getElementById("x").innerHTML = ketqua;
+  // document.querySelector(".ketqua").innerHTML = ketqua;
+  // document.querySelectorAll(".ketqua").innerHTML = ketqua;
+  // document.querySelector("#nghiem-2").innerHTML = ketqua;
 }
 
