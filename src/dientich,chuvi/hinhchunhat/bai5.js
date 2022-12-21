cae97bc9a937587cd439d606b13dc851c9009553
Cuong Le <lequocc31@gmail.com>
2022-12-21 10:27:34 +0700

    JS

--- src/dientich,chuvi/hinhchunhat/bai5.js
+++ src/dientich,chuvi/hinhchunhat/bai5.js
@@ -2,11 +2,7 @@ function tinhdtcv(){
     var dai = parseFloat(document.dataform.dai.value);
     var rong = parseFloat(document.dataform.rong.value);
     var ketqua = "Chu Vi và Diện tích hình chữ nhật có chiều dài: "+dai+" m và chiều rộng: " +rong+ " m là:  <br>";
-    if(dai === 0 || rong === 0){
-        ketqua += "Không có chu vi và diện tích";
-    }else {
         ketqua += "Diện Tích (m2): " + dai*rong + "<br>";
         ketqua += "Chu Vi (m): " + 2*(dai+rong);
-    }
     document.getElementById("ketqua").innerHTML = ketqua ;
 }
